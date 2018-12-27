a411f207ed95ba0e6e245f00960436b7dd5c6453
Pyi Thein Khine <37124353+henptk14@users.noreply.github.com>
2018-12-27 13:31:59 -0800

    Initial setup for MVP pattern

---
+++ app/src/main/java/com/shwepinle/spice_android_proto/screens/netlist/mvcviews/netlistViewMVC.java
@@ -0,0 +1,4 @@
+package com.shwepinle.spice_android_proto.screens.netlist.mvcviews;
+
+public interface netlistViewMVC {
+}
---
+++ app/src/main/java/com/shwepinle/spice_android_proto/screens/result/mvcviews/resultViewMVC.java
@@ -0,0 +1,4 @@
+package com.shwepinle.spice_android_proto.screens.result.mvcviews;
+
+public interface resultViewMVC {
+}
